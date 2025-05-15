48fb14785ac15a547bd52c802f2607368b152c8c
lauvAri <198719993@qq.com>
2025-05-15 19:43:05 +0800

    修改地图显示

--- app/properties/[id]/carousel.jsx
+++ app/properties/[id]/carousel.jsx
@@ -1,23 +1,26 @@
 "use client"
-import Link from "next/link";
+import React from "react";
 import { useState } from "react";
 export default function Carousel({images, width='100%', height='auto'}) {
     const length = images.length; // 图片数量
     if (length === 0) return null; // 空数组不渲染任何
     const [index, setIndex] = useState(0);
     return (
-        <div className="carousel w-full" style={{width, height}}>
-            {
-                <div className="carousel-item relative w-full" >
-                    <img
-                    src={images[index]}
-                    className="w-full" />
-                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                    <span className="btn btn-circle" onClick={()=>setIndex((((index - 1) + images.length)) % images.length)}>❮</span>
-                    <span className="btn btn-circle" onClick={()=>setIndex((index + 1) % images.length)}>❯</span>
+        <div>
+            <div className="carousel w-full" style={{width, height}}>
+                {
+                    <div className="carousel-item relative w-full" >
+                        <img
+                        src={images[index]}
+                        className="w-full rounded-md" />
+                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <span className="btn btn-circle" onClick={()=>setIndex((((index - 1) + images.length)) % images.length)}>❮</span>
+                        <span className="btn btn-circle" onClick={()=>setIndex((index + 1) % images.length)}>❯</span>
+                        </div>
                     </div>
-                </div>
-            }
+                }
+            </div>
+            <p>{index + 1}/{length}</p>
         </div>
     )
 }
--- components/BaiduMap.jsx
+++ components/BaiduMap.jsx
@@ -1,13 +1,15 @@
 "use client";
 
 import React from 'react';
-import { Map, APILoader, Marker } from '@uiw/react-baidu-map';
+import { Map, APILoader, Marker, NavigationControl } from '@uiw/react-baidu-map';
+
 
 const BaiduMap = ({width="100%", height="500px", center="中南大学"}) => (
   <div style={{ width, height, overflow: 'auto' }}>
     <APILoader akay="s8zb8xxc5roTjEZuyQZc0lkBpZJAL1ph">
       <Map center={center}>
-        <Marker position={center} />
+        
+        <NavigationControl />
       </Map>
       
     </APILoader>
